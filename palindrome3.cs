9bfe0f65ca29d5138d74ed81fe8268ad8abf36bc
Ryan Murry <ryanjmurry@gmail.com>
2018-06-26 08:04:24 -0700

    update palindrome3.cs to create object  while moving logic in main to  class

--- palindrome3.cs
+++ palindrome3.cs
@@ -1,42 +1,46 @@
 using System;
 using System.Collections.Generic;
 
+class Palindrome
+{
+    private string _userInput;
+    private int _max;
+    private int _min = 0;
+
+    public bool PalindromeChecker()
+    {
+        Console.WriteLine("Enter a word: ");
+        _userInput = Console.ReadLine();
+        _max = _userInput.Length - 1;
+
+        while(true)
+        {
+          if(_min > _max)
+          {
+            return true;
+          }
+          if(_userInput[_min] != _userInput[_max])
+          {
+            return false;
+          }
+          _max--;
+          _min++;
+        }
+    }
+}
+
 class Program
 {
-  public static bool PalindromeChecker(string userWord)
-  {
-    int y = (userWord.Length -1);
-    int x = 0;
-    while(true)
+    public static void Main()
     {
-      if(x > y)
-      {
-        return true;
-      }
-      if(userWord[x] != userWord[y])
-      {
-        return false;
-      }
-      y--;
-      x++;
+        Palindrome userPalindrome = new Palindrome();
+        if (userPalindrome.PalindromeChecker())
+        {
+            Console.WriteLine("Palindrome!");
+        }
+        else
+        {
+            Console.WriteLine("Nope!");
+        }
     }
-  }
-
-  public static void Main()
-  {
-    Console.WriteLine("Enter a word");
-    string userInput = Console.ReadLine();
-    Console.WriteLine(userInput);
-    if(PalindromeChecker(userInput))
-    {
-      Console.WriteLine("Palindrome!");
-    }
-    else
-    {
-      Console.WriteLine("not a palindrome");
-    }
-
-
-
-  }
 }
